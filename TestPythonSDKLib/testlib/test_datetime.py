e15957dd26495569cb20c95e2caa6f8c68657f31
Kwok <AndyKuo@yeah.net>
2022-09-07 21:58:22 +0800

    Update TestPythonSDKLib About `datetime` Test .
    1.update TestPythonSDKLib\testlib\test_datetime.py .
    2.update TestPythonSDKLib\testlib\test_datetime_timestamp.py .

--- TestPythonSDKLib/testlib/test_datetime.py
+++ TestPythonSDKLib/testlib/test_datetime.py
@@ -1,8 +1,10 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date, time as datetime_time
 
 
 # 时间戳  <->  datetime  <->  时间字符串
 
+
+print("{0} 1-1 {0}".format("*" * 9))
 
 # datetime 转 时间戳
 ts_s = datetime.now().timestamp()
@@ -12,8 +14,7 @@ print(ts_s)
 datetime.fromtimestamp(ts_s)
 
 
-print("*" * 18)
-
+print("{0} 1-2 {0}".format("*" * 9))
 
 # datetime 转 时间字符串
 time_str = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
@@ -23,7 +24,7 @@ print(time_str)
 print(datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S.%f'))
 
 
-print("*" * 18)
+print("{0} 1-3 {0}".format("*" * 9))
 
 
 # 时间戳 [转 datetime] 转 时间字符串
@@ -36,8 +37,40 @@ print(datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S.%f').timestamp())
 print("*" * 36)
 
 
-# 时间计算，保留到毫秒
-print((datetime.now() - timedelta(days=1, hours=2)).strftime("%Y-%m-%d %H:%M:%S.%f")[:-3])
+# 构造 datetime.date
+print(date(2020, 10, 1))
+
+# 构造 datetime.time
+print(datetime_time(20, 10))
+
+# 构造 datetime.datetime
+datetime.combine(date(2020, 10, 1), datetime_time(20, 10))
+print(datetime(2022, 10, 1))
+print(datetime(2022, 10, 1, 9))
+
+
+print("*" * 36)
+
+# 时间戳是指格林威治时间 1970年01月01日00时00分00秒(北京时间1970年01月01日08时00分00秒)起至现在的总秒数。
+print(datetime(1970, 1, 1).isoformat())
+print(datetime(1970, 1, 2, 8, 0, 0).timestamp())
+print(24 * 60 * 60)
+
+
+print("*" * 36)
+
+
+# 修改 datetime 某字段
+print(datetime(2022, 10, 1, 9).replace(year=2050, hour=10))
+
+
+print("*" * 36)
+
+
+print(datetime.today().isoformat())
+print(datetime.utcnow().isoformat())
+# datetime.now(tz) 时区参数可选
+print(datetime.now().isoformat())
 
 
 print("*" * 36)
--- TestPythonSDKLib/testlib/test_datetime_timestamp.py
+++ TestPythonSDKLib/testlib/test_datetime_timestamp.py
@@ -1,12 +1,17 @@
-from datetime import datetime, date, timedelta
+from datetime import datetime, timedelta, date, time as datetime_time
 import time
 
 
 # datetime  <->  [ 时间戳 / 时间结构体 ]  <->  time
 
 
+print("{0} 1-1 {0}".format("*" * 9))
+
 # datetime 转 时间结构体
 print(datetime.now().timetuple())
+
+
+print("{0} 1-2 {0}".format("*" * 9))
 
 # time [转 时间戳] 转 datetime
 print(datetime.fromtimestamp(time.time()))
@@ -17,7 +22,9 @@ print(time.localtime(datetime.now().timestamp()))
 # datetime [转 时间结构体] [转 时间戳] 转 时间结构体
 print(time.localtime(time.mktime(datetime.now().timetuple())))
 
+
 print("*" * 36)
+
 
 # time 获取 当前时间字符串
 print(time.strftime('%Y-%m-%d %H:%M:%S'))
@@ -28,9 +35,7 @@ print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 print("*" * 36)
 
-
-# 今日 0时 时间戳
-print(int(time.mktime(date.today().timetuple())))
+print("{0} 2-1 {0}".format("*" * 9))
 
 # time 获取某日 0 时 秒时间戳
 print(time.mktime(time.strptime("2022-9-10 0:0:0", '%Y-%m-%d %H:%M:%S')))
@@ -38,9 +43,40 @@ print(time.mktime(time.strptime("2022-9-10 0:0:0", '%Y-%m-%d %H:%M:%S')))
 # datetime 获取某日 0 时 秒时间戳
 print(datetime.strptime("2022-9-10 0:0:0", '%Y-%m-%d %H:%M:%S').timestamp())
 
+
+print("{0} 2-2 {0}".format("*" * 9))
+
 # 获取某时间间隔的 0时 秒时间戳
 now = datetime.now()
-print(int((now - timedelta(days=30, hours=now.hour, minutes=now.minute, seconds=now.second,
-                           microseconds=now.microsecond)).timestamp()))
+print((now - timedelta(days=30, hours=now.hour, minutes=now.minute, seconds=now.second,
+                           microseconds=now.microsecond)).timestamp())
+
+
+print("{0} 2-3 {0}".format("*" * 9))
+
+# 当日 0时 秒时间戳
+print(time.mktime(date.today().timetuple()))
+print(datetime.combine(date.today(), datetime_time.min).timestamp())
+print(datetime.today().replace(hour=0, minute=0, second=0, microsecond=0).timestamp())
+
+# 当日 23:59:59 秒时间戳
+print(datetime.combine(date.today(), datetime_time.max).timestamp())
+print(datetime.today().replace(hour=23, minute=59, second=59, microsecond=999999).timestamp())
+
+
+print("*" * 36)
+
+
+# 时间间隔 timedelta 对象，用于时间的计算。
+print(datetime.now() + timedelta(days=0, hours=0, minutes=0, seconds=0,
+                                 milliseconds=0, microseconds=0, weeks=1))
+
+# 时间计算，保留到毫秒
+print((datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S.%f")[:-3])
+
+# datetime +-
+timedelta_obj = datetime(2022, 10, 1, 9) - datetime(2022, 9, 10)
+print(timedelta_obj.days)
+print(timedelta_obj.total_seconds())
 
 
