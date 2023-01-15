61fa4d0c29b4c3aca80397e911712e6625490159
Kwok <AndyKuo@yeah.net>
2023-01-15 14:14:49 +0800

    Update Some Test About urllib.parse、collections.Counter .
    1.update TestPythonSDKLib\testlib\test_collections.py .
    2.update TestPythonSDKLib\testlib\test_urllib_parse.py .

--- TestPythonSDKLib/testlib/test_collections.py
+++ TestPythonSDKLib/testlib/test_collections.py
@@ -6,3 +6,6 @@ l = ["a", 1, "b", 2, 'c', "c", 'd', 3, 3, 2, 3]
 c = collections.Counter(l)
 print(c.most_common(6))
 
+for i in zip(c.keys(), c.values()):
+    print(f"Word: {i[0]}, Count: {i[1]}")
+
--- TestPythonSDKLib/testlib/test_urllib_parse.py
+++ TestPythonSDKLib/testlib/test_urllib_parse.py
@@ -6,3 +6,15 @@ print(parse.quote('http://10.1.188.110:9090/api/v1/query?query=kminion_kafka_bro
 # 解码
 print(parse.unquote('http%3A//10.1.188.110%3A9090/api/v1/query%3Fquery%3Dkminion_kafka_broker_info%7Bjob%3D%22Kafka-jq-01%22%7D%26time%3D1666804596'))
 
+
+print("*" * 36)
+
+
+# 转换路径参数
+data = {
+    "index": 1,
+    "size": 10
+}
+print(parse.urlencode(data))
+
+
