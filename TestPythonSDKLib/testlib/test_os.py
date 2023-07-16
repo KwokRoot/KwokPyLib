9b33feddd04c3bb9083a5a103db775ccfcc6dfe3
Kwok <AndyKuo@yeah.net>
2023-07-16 17:56:35 +0800

    Update
    1.update TestPythonSDKLib\testlib\test_os.py .
    2.update TestPythonSDKLib\testlib\test_sys.py .

--- TestPythonSDKLib/testlib/test_os.py
+++ TestPythonSDKLib/testlib/test_os.py
@@ -1,16 +1,30 @@
 import os
 
+print(f'{"*" * 9} 获取当前路径 {"*" * 9}')
+
+current_dir = os.getcwd();
+print(current_dir)
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+print(base_path)
+
+
+print(f'{"*" * 9} 创建、移动(重命名)、删除文件夹 {"*" * 9}')
+
 try:
     os.makedirs("/opt/1/11/111", exist_ok=True)
-    os.rename("/opt/1/11", "/opt/2")
+
+    # os.rename("/opt/1/11", "/opt/2")
+    # 删除多层空目录，会递归删除上层空目录
+    # os.removedirs("/opt/2/222")
+    # 删除空目录，不会删除上层的空目录
+    # os.rmdir("/opt/2/111", )
 except OSError as e:
-    print(e)
+    print("抛 OSError 异常：", e)
     pass
 
 
-print("*" * 36)
-
-
+print(f'{"*" * 9} 修改 cmd 编码 {"*" * 9}')
 # 将 cmd 显示的字符编码从默认的 GBK 改为 UTF-8
 os.system('chcp 65001')
 res = os.system("dir")
@@ -18,25 +32,20 @@ os.system("python -V")
 print(res)
 
 
-print("*" * 36)
-
+print(f'{"*" * 9} 创建日志目录示例 {"*" * 9}')
 
 # base_path = os.getcwd()
 base_path = os.path.dirname(os.path.abspath(__file__))
-print(base_path)
-
+# print(base_path)
 my_path = os.path.join(base_path, "logs")
 if not os.path.exists(my_path):
     os.makedirs(my_path, exist_ok=True)
-
 my_log_path = os.path.join(my_path, "log.txt")
-
 with open(my_log_path, "w+", encoding="utf-8") as f:
     pass
 
 
-print("*" * 36)
-
+print(f'{"*" * 9} 获取文件描述符 {"*" * 9}')
 
 # 获取文件描述符(文件标识)，经测试，Windows、linux 同一逻辑磁盘下，重命名、移动都不会发生改变，可用于日志采集。
 # Python 在 Windows 系统、Linux 系统该方式均适用。
@@ -44,8 +53,7 @@ file_info = os.stat(my_log_path)
 print(file_info)
 
 
-print("*" * 36)
-
+print(f'{"*" * 9} 遍历文件目录 {"*" * 9}')
 
 # 遍历文件目录
 for root, dirs, files in os.walk("/opt/", topdown=True):
@@ -53,13 +61,11 @@ for root, dirs, files in os.walk("/opt/", topdown=True):
         print(os.path.join(root, name))
 
 
-print("*" * 36)
-
+print(f'{"*" * 9} 执行 shell 命令 {"*" * 9}')
 
 # 执行 shell 命令
 os.system("dir")
-print("*" * 18)
+print("*" * 36)
 # 解决中文文件名乱码
 print(os.popen("dir").read())
-
 
--- TestPythonSDKLib/testlib/test_sys.py
+++ TestPythonSDKLib/testlib/test_sys.py
@@ -1,21 +1,21 @@
 import sys
 
+print(f'{"*" * 9} 获取当前 Python 环境路径 {"*" * 9}')
 print(">>>" + str(sys.path))
 
 
-print("*" * 36)
+print(f'{"*" * 9} 获取 Python 启动加载的模块 {"*" * 9}')
+print(sys.modules)
 
 
-# 获取 Python 执行时输入参数
-print(len(sys.argv))
-print(sys.argv)
-print(sys.argv[0])
+print(f'{"*" * 9} 获取 Python 版本信息 {"*" * 9}')
 
+# 获取 Python 版本
+print(sys.version)
+# 获取操作系统平台
+print(sys.platform)
 
-print("*" * 36)
-
-
-# 获取 Python 版本信息
+# 获取 Python 详细版本信息
 print(sys.version_info)
 print(f"{sys.version_info.major}.{sys.version_info.minor}")
 
@@ -23,7 +23,19 @@ this_python = sys.version_info[:2]
 min_version = (3, 6)
 if this_python < min_version:
     print("ERROR: " + " ".join("python 版本不符合要求."))
-    exit(1)
+    sys.exit(-1)
+
+
+print(f'{"*" * 9} 获取 Python 执行时输入的参数 {"*" * 9}')
+
+# 获取 Python 执行时输入的参数
+print(len(sys.argv))
+print(sys.argv)
+print(sys.argv[0])
+
+
+print(f'{"*" * 9} 获取系统编码 {"*" * 9}')
+print(sys.getdefaultencoding())
 
 
 if __name__ == '__main__':
