2b8a4d63f25c91dbf81c004ca487f70e0f90fcdc
Kwok <AndyKuo@yeah.net>
2023-09-10 17:08:43 +0800

    Update test_redis.py

--- TestPyCommonLib/test3lib/test_redis.py
+++ TestPyCommonLib/test3lib/test_redis.py
@@ -2,9 +2,11 @@ import redis
 
 # # redis 连接池
 # redis_pool = redis.ConnectionPool(host='127.0.0.1', port=6379, password="123456", db=0, decode_responses=True)
+# redis_pool = redis.ConnectionPool.from_url("redis://:123456@127.0.0.1:6379/0", encoding="UTF-8", decode_responses=True)
 # r = redis.Redis(connection_pool=redis_pool)
 
 r = redis.Redis(host="127.0.0.1", port=6379, password="123456", db=0, decode_responses=True)
+# r = redis.Redis.from_url("redis://:123456@127.0.0.1:6379/0", encoding="UTF-8", decode_responses=True)
 
 print("Pong: " + str(r.ping()))
 
